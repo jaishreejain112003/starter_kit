e25795f0b5eb92e2c48db4b816962c2be10cb1b9
jaishreejain112003 <vijayshrijain2003@gmail.com>
2024-10-30 11:02:59 +0530

    completed part 1

---
+++ src/contracts/Marketplace.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.5.0;
+
+contract Marketplace {
+    string public name;
+
+    struct Product{
+        uint id;
+        string name;
+        uint price;
+        address owner;
+        bool purchased;
+    }
+    
+
+    constructor() public {
+        name = "Dapp University Marketplace";
+    }
+
+}
