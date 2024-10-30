65cae9b96ea10e7a2fa19a6260e536339ca1278a
jaishreejain112003 <vijayshrijain2003@gmail.com>
2024-10-30 12:54:47 +0530

    part2_product_listed

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.5.0;
 
 contract Marketplace {
     string public name;
+    uint public productCount = 0;
+    mapping(uint => Product) public products;
 
     struct Product{
         uint id;
@@ -10,10 +12,34 @@ contract Marketplace {
         address owner;
         bool purchased;
     }
+    event ProductCreated(
+        uint id,
+        string name,
+        uint price,
+        address owner,
+        bool purchased
+    );
     
 
     constructor() public {
         name = "Dapp University Marketplace";
+    }
+
+    function createProduct(string memory _name , uint _price) public{
+        // Make sure that the function parameters are correct 
+        // Create the product 
+        // Increment productCount
+        // Trigger an event 
+        require(bytes(_name).length > 0);
+        require(_price > 0);
+
+        productCount++;
+        products[productCount] = Product(productCount,_name,_price,msg.sender,false);
+        emit ProductCreated(productCount,_name,_price,msg.sender,false);
+
+
+
+
     }
 
 }
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,7 +1,13 @@
 const Marketplace = artifacts.require("./Marketplace.sol");
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+
 // why is the address like this 
 
-contract('Marketplace' , (accounts)=> {
+contract('Marketplace' , ([deployer,seller,buyer])=> {
     let marketplace
 
     before(async ()=> {
@@ -22,4 +28,34 @@ contract('Marketplace' , (accounts)=> {
             assert.equal(name,'Dapp University Marketplace')
         })
     })
+
+    describe('products', async () => {
+        let result, productCount
+
+        before(async ()=> {
+            
+            result = await marketplace.createProduct("macbook", web3.utils.toWei('1','Ether'))
+            productCount = await marketplace.productCount()
+            
+        })
+        it('creates products', async () =>{
+            assert.equal(productCount,1)
+            const event1 = result.logs[0].args
+            console.log(result.logs[0])
+            assert.equal(event1.id.toNumber(),productCount.toNumber(),'id is correct');
+            assert.equal(event1.name,'macbook','name is correct');
+            assert.equal(event1.price,'1000000000000000000','price is correct');
+            assert.equal(event1.owner,result.logs[0].args.owner , 'owner is correct');
+            assert.equal(event1.purchased,false,'event is correct');
+
+            // FAILURE
+
+            await marketplace.createProduct('hey',0).should.be.rejected;
+
+
+
+        })
+    })
+
+
 })
